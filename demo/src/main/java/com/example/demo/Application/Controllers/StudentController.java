1458c4b2af0dca4c06d4326b495392bbd20e1f0a
Ishini Dewamiththa <ishinidewamiththa@gmail.com>
2025-04-04 02:05:33 +0530

    add a controller

---
+++ demo/src/main/java/com/example/demo/Application/Controllers/StudentController.java
@@ -0,0 +1,10 @@
+package com.example.demo.Application.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController     //req res handle krna hdpu controller ekk kyla spring boot wlt kynw
+                    //me annotation ek haraha
+@RequestMapping("/student")
+public class StudentController {
+}
