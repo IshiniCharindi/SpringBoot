d049f08d8378769f81f011467c4ff227d254fc5c
Ishini Dewamiththa <ishinidewamiththa@gmail.com>
2025-04-05 00:53:11 +0530

    depedency update

--- demo/src/main/java/com/example/demo/Domain/Service/StudentService.java
+++ demo/src/main/java/com/example/demo/Domain/Service/StudentService.java
@@ -1,11 +1,20 @@
 package com.example.demo.Domain.Service;
 
 import com.example.demo.Application.DTO.Response.StudentGeneralDTO;
+import com.example.demo.External.Repository.StudentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.modelmapper.ModelMapper;
 
 @Service
+@AllArgsConstructor
 public class StudentService {
+
+    private StudentRepository studentRepository;
+
+    private ModelMapper modelMapper;
     public ResponseEntity<StudentGeneralDTO> getStudent(Integer id) {
 
     }
